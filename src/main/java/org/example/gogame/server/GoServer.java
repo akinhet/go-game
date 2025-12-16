c38d0451d863851a6110c7e553511c691a291d79
akinhet <piotrzieniewicz2@gmail.com>
2025-12-16 14:01:01 +0100

    Add GoServer.java

---
+++ src/main/java/org/example/gogame/server/GoServer.java
@@ -0,0 +1,58 @@
+package org.example.gogame.server;
+
+import org.example.gogame.StoneColor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class GoServer {
+
+    private static int port = 1111;
+
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            try {
+                port = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.err.println("Not a port number: " + args[1]);
+            }
+        } else if (args.length == 1) {
+            new GoServer().start();
+        } else {
+            System.err.println("Malformed arguments. Exiting...");
+        }
+    }
+
+    public void start() {
+        System.out.println("Go Server is running on port " + port);
+
+        try (ServerSocket listener = new ServerSocket(port)) {
+            while (true) {
+                System.out.println("Waiting for Player 1 (BLACK)...");
+                Socket socket1 = listener.accept();
+                PlayerHandler player1 = new PlayerHandler(socket1, StoneColor.BLACK);
+                player1.sendMessage("MESSAGE Connected as BLACK. Waiting for opponent...");
+
+                System.out.println("Player 1 connected. Waiting for Player 2 (WHITE)...");
+                Socket socket2 = listener.accept();
+                PlayerHandler player2 = new PlayerHandler(socket2, StoneColor.WHITE);
+                player2.sendMessage("MESSAGE Connected as WHITE. Game starting...");
+
+                System.out.println("Both players connected. Starting game.");
+                Game game = new Game(player1, player2, 0);
+
+                player1.setGame(game);
+
+                player2.setGame(game);
+
+                new Thread(player1).start();
+                new Thread(player2).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Server error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
